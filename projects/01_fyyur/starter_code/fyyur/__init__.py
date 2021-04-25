44008bc4867c2c956ed1ee6115a00e1c20ca9bd9
Felix Stubner <Fail2Reap@users.noreply.github.com>
2021-04-25 20:32:47 +0100

    Refactoring to allow for models to be defined in its own module

---
+++ projects/01_fyyur/starter_code/fyyur/__init__.py
@@ -0,0 +1,38 @@
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+import dateutil.parser
+import babel
+from flask import Flask
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+#----------------------------------------------------------------------------#
+# App Config
+#----------------------------------------------------------------------------#
+app = Flask(__name__)
+app.config.from_object('../config')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+moment = Moment(app)
+
+from fyyur import routes
+
+# TODO: connect to a local postgresql database - DONE
+
+
+
+#----------------------------------------------------------------------------#
+# Filters
+#----------------------------------------------------------------------------#
+def format_datetime(value, format='medium'):
+  date = dateutil.parser.parse(value)
+  if format == 'full':
+      format="EEEE MMMM, d, y 'at' h:mma"
+  elif format == 'medium':
+      format="EE MM, dd, y h:mma"
+  return babel.dates.format_datetime(date, format, locale='en')
+
+app.jinja_env.filters['datetime'] = format_datetime
---
+++ projects/01_fyyur/starter_code/fyyur/models.py
@@ -0,0 +1,50 @@
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+from fyyur import db
+
+
+#----------------------------------------------------------------------------#
+# Models
+#----------------------------------------------------------------------------#
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    # genres
+    facebook_link = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    # website link
+    # looking for talent
+    # seeking description
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    # website link
+    # looking for venues
+    # seeking description
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+class Show(db.Model):
+  __tablename__ = 'Shows'
+
+  id = db.Column(db.Integer, primary_key=True)
